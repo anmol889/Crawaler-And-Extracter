aadca9b9ff5763954e24d5f2af59eb3ee45d629b
anmolpathak98 <anmol.pathak@turtlemint.com>
2021-05-31 00:15:03 +0530

    added time taken feature for api

--- src/main/java/com/crawler/extracter/controller/ScrapperController.java
+++ src/main/java/com/crawler/extracter/controller/ScrapperController.java
@@ -1,18 +1,12 @@
 package com.crawler.extracter.controller;
 
-
-import com.crawler.extracter.model.PriceTrend;
-import com.crawler.extracter.model.ProductDetails;
 import com.crawler.extracter.model.ProductDetailsRequest;
 import com.crawler.extracter.service.ScrapperService;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -23,27 +17,52 @@ public class ScrapperController {
     ScrapperService scrapperService;
 
     @GetMapping("/getProductDetails/{productId}")
-    public ProductDetails gettingProductDetails(@PathVariable final String productId) {
-        return scrapperService.fetchingProductDetails(productId);
+    public Map<String, Object> gettingProductDetails(@PathVariable final String productId) {
+        Long from = System.currentTimeMillis();
+        Map<String, Object> productDetailsMap = new HashMap<>();
+        productDetailsMap.put("productDetails", scrapperService.fetchingProductDetails(productId));
+        Long to = System.currentTimeMillis();
+        productDetailsMap.put("timeTaken", (to - from)/1000.0+" seconds");
+        return productDetailsMap;
     }
 
     @GetMapping("/allCrawledProducts")
-    public List<ProductDetails> allCrawledProducts() {
-        return scrapperService.findAllCrawledProducts();
+    public Map<String, Object> allCrawledProducts() {
+        Long from = System.currentTimeMillis();
+        Map<String, Object> crawledDetails = new HashMap<>();
+        crawledDetails.put("ProductDetails", scrapperService.findAllCrawledProducts());
+        Long to = System.currentTimeMillis();
+        crawledDetails.put("timeTaken",(to - from)/1000.0+" seconds");
+        return crawledDetails;
     }
 
     @GetMapping("/priceTrend/{productId}")
-    public List<PriceTrend> priceTrend(@PathVariable final String productId) {
-        return scrapperService.gettingPriceTrend(productId);
+    public Map<String, Object> priceTrend(@PathVariable final String productId) {
+        Long from = System.currentTimeMillis();
+        Map<String, Object> priceTrendsMap = new HashMap<>();
+        priceTrendsMap.put("priceTrends",scrapperService.gettingPriceTrend(productId));
+        Long to = System.currentTimeMillis();
+        priceTrendsMap.put("timeTaken",(to - from)/1000.0+" seconds");
+        return  priceTrendsMap;
     }
 
     @PostMapping("/getProductDetailsHistory")
-    public ResponseEntity<?> getProductDetailsHistory(@RequestBody ProductDetailsRequest productDetailsRequest) throws ParseException {
-        return scrapperService.fetchingProductDetailsHistory(productDetailsRequest.getTimestamp(), productDetailsRequest.getProductId());
+    public Map<String, Object> getProductDetailsHistory(@RequestBody ProductDetailsRequest productDetailsRequest) throws ParseException {
+        Long from = System.currentTimeMillis();
+        Map<String, Object> productDetailsHistoryMap = new HashMap<>();
+        productDetailsHistoryMap.put("productDetail",scrapperService.fetchingProductDetailsHistory(productDetailsRequest.getTimestamp(), productDetailsRequest.getProductId()).getBody());
+        Long to = System.currentTimeMillis();
+        productDetailsHistoryMap.put("timeTaken",(to - from)/1000.0+" seconds");
+        return productDetailsHistoryMap;
     }
 
     @GetMapping("/getHtml/{productId}")
-    public String fetchingHtmlPage(@PathVariable final String productId) {
-        return scrapperService.scrappingHtmlPage(productId);
+    public Map<String, Object> fetchingHtmlPage(@PathVariable final String productId) {
+        Long from = System.currentTimeMillis();
+        Map<String, Object> htmlPageMap = new HashMap<>();
+        htmlPageMap.put("pageSource",scrapperService.scrappingHtmlPage(productId));
+        Long to = System.currentTimeMillis();
+        htmlPageMap.put("timeTaken",(to - from)/1000.0+" seconds");
+        return htmlPageMap;
     }
 }
